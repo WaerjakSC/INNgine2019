414350db43d463a71c86ff702b5277d7964e03eb
gangestad <36534289+gangestad@users.noreply.github.com>
2019-09-05 11:43:24 +0200

    Includes for components
    contains includes for all components

---
+++ Components/comppch.h
@@ -0,0 +1,13 @@
+#ifndef COMPPCH_H
+#define COMPPCH_H
+
+// Component headers
+#include "Components/meshcomponent.h"
+#include "Components/inputcomponent.h"
+#include "Components/lightingcomponent.h"
+#include "Components/materialcomponent.h"
+#include "Components/physicscomponent.h"
+#include "Components/soundcomponent.h"
+#include "Components/transformcomponent.h"
+
+#endif // COMPPCH_H
