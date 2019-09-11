f569c01d0c3d7e691a26eb150502397836017d12
gangestad <36534289+gangestad@users.noreply.github.com>
2019-09-12 00:14:49 +0200

    re-commit transformcomponent

--- Components/transformcomponent.cpp
+++ Components/transformcomponent.cpp
@@ -1,6 +1,62 @@
 #include "transformcomponent.h"
 
-TransformComponent::TransformComponent()
+TransformComponent::TransformComponent() {
+    mType = CType::Transform;
+}
+
+void TransformComponent::update(float dt) {
+
+}
+
+gsl::Matrix4x4 *TransformComponent::matrix() {
+    if(mMatrixOutdated){
+            updateMatrix();
+    }
+     return &mMatrix;
+}
+
+void TransformComponent::setPosition(gsl::Vector3D &position)
+{
+    mMatrixOutdated = true;
+    mPosition = position;
+}
+
+void TransformComponent::setPosition(float xIn, float yIn, float zIn)
+{
+     mMatrixOutdated = true;
+     mPosition.x = xIn;
+     mPosition.y = yIn;
+     mPosition.z = zIn;
+}
+
+void TransformComponent::setScale(gsl::Vector3D &scale)
+{
+    mMatrixOutdated = true;
+    mScale = scale;
+}
+
+void TransformComponent::setRotation(gsl::Vector3D &rotation)
+{
+    mMatrixOutdated = true;
+    mRotation = rotation;
+}
+
+gsl::Matrix4x4 TransformComponent::multiplyByParent(gsl::Matrix4x4 matrixIn)
 {
 
+}
+
+void TransformComponent::updateMatrix()
+{
+    if(mMatrixOutdated)
+     {
+         //calculate matrix from position, scale, rotation
+         mMatrix.identity();
+         mMatrix.translate(mPosition);
+         mMatrix.rotateX(mRotation.x);
+         mMatrix.rotateY(mRotation.y);
+         mMatrix.rotateZ(mRotation.z);
+         mMatrix.scale(mScale);
+         mMatrixOutdated = false;
+     }
 }
--- Components/transformcomponent.h
+++ Components/transformcomponent.h
@@ -2,10 +2,36 @@
 #define TRANSFORMCOMPONENT_H
 
 #include "component.h"
+#include "vector3d.h"
 
 class TransformComponent : public Component {
 public:
     TransformComponent();
+    virtual void update(float dt = 0.0f);
+    bool mMatrixOutdated{true};
+
+    gsl::Matrix4x4 *matrix();
+
+    gsl::Vector3D mPosition{0};
+    gsl::Vector3D mRotation{0};
+    gsl::Vector3D mScale{0};
+
+    int parentID = -1;
+
+    // Move to movement system later, transform component should only hold the values of position, rotation and scale
+    // Thanks Ole! <3
+    void setPosition(gsl::Vector3D &position);
+    void setPosition(float xIn, float yIn, float zIn);
+    void setScale(gsl::Vector3D &scale);
+    void setRotation(gsl::Vector3D &rotation);
+
+    gsl::Matrix4x4 multiplyByParent(gsl::Matrix4x4 matrixIn);
+    void updateMatrix();
+
+private:
+    gsl::Matrix4x4 mMatrix;
+
+
 };
 
 #endif // TRANSFORMCOMPONENT_H
