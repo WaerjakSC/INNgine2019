b332dca817e892baa7090772123e494a3f5500f2
gangestad <gangestad@protonmail.ch>
2019-11-11 18:03:03 +0100

    vsim curves and points and full control

--- ECS/components.h
+++ ECS/components.h
@@ -300,16 +300,7 @@ struct Cylinder : public Collision {
  * @brief The BSplineCurve struct
  */
 struct BSplineCurve : Component {
-    std::vector<vec3> b;  // control points
-    int n;                // n number of knots
-    int d;                // d degrees
-    std::vector<float> t; // knots
 
-    // default constructor
-    BSplineCurve() {}
-    BSplineCurve(std::vector<float> knots, std::vector<vec3> controlpoints, int degree = 2) : b(controlpoints), d(degree), t(knots) {
-        n = knots.size();
-    }
 };
 
 // TD enemies følger en path fra A til B
